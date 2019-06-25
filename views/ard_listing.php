074bcca0cf794a634eb0a651ca60cfa5855f3e51
Arjen van Bochoven <nejra@me.com>
2019-06-26 01:06:22 +0200

    Update ard_listing.php
    Change to new format

--- views/ard_listing.php
+++ views/ard_listing.php
@@ -1,78 +1,41 @@
-<?php $this->view('partials/head'); ?>
+<?php 
 
-<?php //Initialize models needed for the table
-new Machine_model;
-new Reportdata_model;
-new Ard_model;
-?>
-
-<div class="container">
-
-  <div class="row">
-
-  	<div class="col-lg-12">
-
-		  <h3><span data-i18n="ard.listing.title"></span> <span id="total-count" class='label label-primary'>…</span></h3>
-
-		  <table class="table table-striped table-condensed table-bordered">
-		    <thead>
-		      <tr>
-		      	<th data-i18n="listing.computername" data-colname='machine.computer_name'></th>
-		        <th data-i18n="serial" data-colname='reportdata.serial_number'></th>
-		        <th data-i18n="username" data-colname='reportdata.long_username'></th>
-		        <th data-i18n="ard.listing.text" data-i18n-options='{"number":1}' data-colname='ard.text1'></th>
-		        <th data-i18n="ard.listing.text" data-i18n-options='{"number":2}' data-colname='ard.text2'></th>
-		        <th data-i18n="ard.listing.text" data-i18n-options='{"number":3}' data-colname='ard.text3'></th>
-		        <th data-i18n="ard.listing.text" data-i18n-options='{"number":4}' data-colname='ard.text4'></th>
-		      </tr>
-		    </thead>
-		    <tbody>
-		    	<tr>
-					<td data-i18n="listing.loading" colspan="7" class="dataTables_empty"></td>
-				</tr>
-		    </tbody>
-		  </table>
-    </div> <!-- /span 12 -->
-  </div> <!-- /row -->
-</div>  <!-- /container -->
-
-<script type="text/javascript">
-
-	$(document).on('appUpdate', function(e){
-
-		var oTable = $('.table').DataTable();
-		oTable.ajax.reload();
-		return;
-
-	});
-
-	$(document).on('appReady', function(e, lang) {
-		// Get column names from data attribute
-		var columnDefs = [],
-            col = 0; // Column counter
-		$('.table th').map(function(){
-              columnDefs.push({name: $(this).data('colname'), targets: col});
-              col++;
-		});
-	    oTable = $('.table').dataTable( {
-	        columnDefs: columnDefs,
-	        ajax: {
-                url: appUrl + '/datatables/data',
-                type: "POST"
-            },
-            dom: mr.dt.buttonDom,
-            buttons: mr.dt.buttons,
-	        createdRow: function( nRow, aData, iDataIndex ) {
-	        	// Update name in first column to link
-	        	var name=$('td:eq(0)', nRow).html();
-	        	if(name == ''){name = "No Name"};
-	        	var sn=$('td:eq(1)', nRow).html();
-	        	var link = mr.getClientDetailLink(name, sn);
-	        	$('td:eq(0)', nRow).html(link);
-
-	        }
-	    });
-	});
-</script>
-
-<?php $this->view('partials/foot'); ?>
+$this->view('listings/default',
+[
+  "i18n_title" => 'ard.listing.title',
+  "table" => [
+    [
+      "column" => "machine.computer_name",
+      "i18n_header" => "listing.computername",
+      "formatter" => "clientDetail",
+    ],
+    [
+      "column" => "reportdata.serial_number",
+      "i18n_header" => "serial",
+    ],
+    [
+      "column" => "reportdata.long_username",
+      "i18n_header" => "username",
+    ],
+    [
+      "column" => "ard.text1",
+      "i18n_header" => "ard.listing.text",
+      "i18n-options" => '{"number":1}',
+    ],
+    [
+      "column" => "ard.text2",
+      "i18n_header" => "ard.listing.text",
+      "i18n-options" => '{"number":2}',
+    ],
+    [
+      "column" => "ard.text3",
+      "i18n_header" => "ard.listing.text",
+      "i18n-options" => '{"number":3}',
+    ],
+    [
+      "column" => "ard.text4",
+      "i18n_header" => "ard.listing.text",
+      "i18n-options" => '{"number":4}',
+    ],
+  ]
+]);
