6365d807b0e00f4736691bb409537501a72ed696
Arjen van Bochoven <nejra@me.com>
2019-09-21 12:01:27 +0200

    Add ard_detail_widget

--- provides.php
+++ provides.php
@@ -1,7 +1,10 @@
 <?php
 
-return array(
-    'listings' => array(
-        'ard' => array('view' => 'ard_listing', 'i18n' => 'ard.ard'),
-    ),
-);
+return [
+    'listings' => [
+        'ard' => ['view' => 'ard_listing', 'i18n' => 'ard.ard'],
+    ],
+    'detail_widgets' => [
+        'ard' => ['view' => 'ard_detail_widget'],
+    ],
+];
